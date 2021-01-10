293beb33352f66ef50a5e53257bea49bf546954a
HyunIl <khi0526_@naver.com>
2021-01-10 19:07:23 +0900

    Init New/Open

---
+++ UserInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calibration
+{
+    class UserInfo
+    {
+        public string Name;
+        //public string MacAddr;
+        //public string COMPort;
+        //public string Value;
+        public UserInfo()
+        {
+            Name = "";
+        }
+    }
+}
