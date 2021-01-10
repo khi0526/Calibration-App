322383fd1ba7263a9b165841384f9b5a6ef388d4
HyunIl <khi0526_@naver.com>
2021-01-10 19:08:51 +0900

    add Some Event

--- Form_Main.cs
+++ Form_Main.cs
@@ -13,12 +13,20 @@ namespace Calibration
     public partial class Form_Main : Form
     {
         private const int USER_SIZE = 10;
+
         private UserInfo[] User;
+
 
         public Form_Main()
         {
             InitializeComponent();
             Init();
+
+            User = new UserInfo[USER_SIZE];
+            for(var i = 0; i < USER_SIZE; i++)
+            {
+                User[i] = new UserInfo();
+            }
         }
 
         public void Test()
@@ -35,9 +43,19 @@ namespace Calibration
             InitUI();
         }
 
+        private void ToolStripMenuItem_File_New_Click(object sender, EventArgs e)
+        {
+            NewFileUI();
+        }
+
         private void button_Create_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+            SaveClickUI();
         }
 
         private void button_Connect_Click(object sender, EventArgs e)
