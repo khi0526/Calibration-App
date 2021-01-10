c442bb16f95acccb84b9f369d33852c0084ca7ae
HyunIl <khi0526_@naver.com>
2021-01-10 22:56:30 +0900

    add bool Using

--- UserInfo.cs
+++ UserInfo.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Calibration
 {
     class UserInfo
     {
         public string Name;
-        //public string MacAddr;
-        //public string COMPort;
-        //public string Value;
+        public string Address;
+        public string Port;
+        public string Value;
+
         public UserInfo()
         {
             Name = "";
+            Address = "";
+            Port = "";
+            Value = "";
+        }
+
+        public UserInfo(string file, int index)
+        {
+            var rd = new StreamReader(file);
+            string line;
+            while(!rd.EndOfStream)
+            {
+                line = rd.ReadLine();
+                if(line.Contains($"Device{index}"))
+                {
+                    Name = rd.ReadLine().Split('=')[1];
+                    Address = rd.ReadLine().Split('=')[1];
+                    Port = rd.ReadLine().Split('=')[1];
+                    Value = rd.ReadLine().Split('=')[1];
+
+                    break;
+                }
+            }
         }
     }
 }
