8e5520e5a27ce9cc9bbd95cd28e102bb458e5685
현일 김 <khi0526_@outlook.kr>
2021-01-23 16:31:00 +0900

    change name

--- DeviceInfo.cs
+++ DeviceInfo.cs
@@ -7,25 +7,34 @@ using System.IO;
 
 namespace Calibration
 {
-    class UserInfo
+    class DeviceInfo
     {
         public string Name;
         public string Address;
         public string Port;
-        public string Value;
+        public double Value;
 
-        public UserInfo()
+        public bool Using;
+        public bool Connecting;
+        public bool[,] Complete = new bool[3, 3];
+        public int[] Dose;
+
+        public DeviceInfo()
         {
             Name = "";
             Address = "";
             Port = "";
-            Value = "";
+            Value = 0;
+
+            Using = false;
+            Connecting = false;
         }
 
-        public UserInfo(string file, int index)
+        public DeviceInfo(string file, int index)
         {
             var rd = new StreamReader(file);
             string line;
+
             while(!rd.EndOfStream)
             {
                 line = rd.ReadLine();
@@ -34,11 +43,13 @@ namespace Calibration
                     Name = rd.ReadLine().Split('=')[1];
                     Address = rd.ReadLine().Split('=')[1];
                     Port = rd.ReadLine().Split('=')[1];
-                    Value = rd.ReadLine().Split('=')[1];
+                    Value = Convert.ToDouble(rd.ReadLine().Split('=')[1]);
 
                     break;
                 }
             }
+
+            Connecting = false;
         }
     }
 }
