70768f67e80562cd70244f238ab5cfaef5bc3e7c
현일 김 <khi0526_@outlook.kr>
2021-01-23 16:30:25 +0900

    UI finish

--- Form_Create.Designer.cs
+++ Form_Create.Designer.cs
@@ -29,12 +29,81 @@ namespace Calibration
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.chkListBox_Device = new System.Windows.Forms.CheckedListBox();
+            this.button_Apply = new System.Windows.Forms.Button();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.label_Device = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chkListBox_Device
+            // 
+            this.chkListBox_Device.FormattingEnabled = true;
+            this.chkListBox_Device.Items.AddRange(new object[] {
+            "Device1",
+            "Device2",
+            "Device3",
+            "Device4",
+            "Device5",
+            "Device6",
+            "Device7",
+            "Device8",
+            "Device9",
+            "Device10"});
+            this.chkListBox_Device.Location = new System.Drawing.Point(14, 24);
+            this.chkListBox_Device.Name = "chkListBox_Device";
+            this.chkListBox_Device.Size = new System.Drawing.Size(317, 164);
+            this.chkListBox_Device.TabIndex = 0;
+            // 
+            // button_Apply
+            // 
+            this.button_Apply.Location = new System.Drawing.Point(145, 194);
+            this.button_Apply.Name = "button_Apply";
+            this.button_Apply.Size = new System.Drawing.Size(90, 23);
+            this.button_Apply.TabIndex = 1;
+            this.button_Apply.Text = "Apply";
+            this.button_Apply.UseVisualStyleBackColor = true;
+            this.button_Apply.Click += new System.EventHandler(this.button_Apply_Click);
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.Location = new System.Drawing.Point(241, 194);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(90, 23);
+            this.button_Cancel.TabIndex = 2;
+            this.button_Cancel.Text = "Cancel";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // label_Device
+            // 
+            this.label_Device.AutoSize = true;
+            this.label_Device.Location = new System.Drawing.Point(12, 9);
+            this.label_Device.Name = "label_Device";
+            this.label_Device.Size = new System.Drawing.Size(154, 12);
+            this.label_Device.TabIndex = 3;
+            this.label_Device.Text = "Select devices to calibrate";
+            // 
+            // Form_Create
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form_Create";
+            this.ClientSize = new System.Drawing.Size(341, 222);
+            this.Controls.Add(this.label_Device);
+            this.Controls.Add(this.button_Cancel);
+            this.Controls.Add(this.button_Apply);
+            this.Controls.Add(this.chkListBox_Device);
+            this.Name = "Form_Create";
+            this.Text = "2021.01.13. Calibration Ver.2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox chkListBox_Device;
+        private System.Windows.Forms.Button button_Apply;
+        private System.Windows.Forms.Button button_Cancel;
+        private System.Windows.Forms.Label label_Device;
     }
 }
--- Form_Create.cs
+++ Form_Create.cs
@@ -12,9 +12,42 @@ namespace Calibration
 {
     public partial class Form_Create : Form
     {
-        public Form_Create()
+        public bool[] Using { get; private set; }
+        public bool Creating { get; private set; }
+
+        public Form_Create(string fileName, int version, int usingSize, string[] DeviceNames)
         {
             InitializeComponent();
+
+            this.ControlBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            StartPosition = FormStartPosition.CenterParent;
+            this.Text = $"{fileName} Ver.{version + 1}";
+
+            chkListBox_Device.Items.Clear();
+
+            for (var i = 0; i < usingSize; i++)
+            {
+                chkListBox_Device.Items.Add(DeviceNames[i]);
+                chkListBox_Device.SetItemChecked(i, true);
+            }
+            chkListBox_Device.SelectedIndexChanged += (s, e) => { Using[chkListBox_Device.SelectedIndex] = true; };
+
+            Using = new bool[usingSize];
+            Creating = false;
         }
+
+        private void button_Apply_Click(object sender, EventArgs e)
+        {
+            for (var i = 0; i < chkListBox_Device.CheckedItems.Count; i++)
+            {
+                Using[chkListBox_Device.CheckedIndices[i]] = true;
+            }
+            
+            Creating = true;
+            Close();
+        }
+
+        private void button_Cancel_Click(object sender, EventArgs e) => Close();
     }
 }
